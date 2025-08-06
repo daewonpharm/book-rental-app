bb6cf43e2bf77fc5889db5810567ab16afa36798
unknown <222085@daewonpharm.com>
2025-08-06 15:32:00 +0900

    원복해

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,1 @@
 module.exports = { content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], theme: { extend: {}, }, plugins: [], } 
-// tailwind.config.js
-module.exports = {
-  darkMode: false, // ✅ 다크모드 비활성화
-  // ...
-};
